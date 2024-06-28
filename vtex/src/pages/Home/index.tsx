40ce25905e2b14962cc24631ce564b26578a8fbb
Mr Punk da Silva <gustavojs417@gmail.com>
2024-06-27 21:19:15 -0300

    Arquivo da Página Home criada

---
+++ vtex/src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+const Home = (): ReactNode => {
+  return (
+    <div className="app-home">
+      <aside className=""></aside>
+
+      <header className=""></header>
+
+      <main className=""></main>
+
+      <footer className=""></footer>
+    </div>
+  );
+};
+
+export default Home;
