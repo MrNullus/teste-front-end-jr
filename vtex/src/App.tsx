6a245db5d0b4bab121b842ace91510c7b29c902f
Mr Punk da Silva <gustavojs417@gmail.com>
2024-06-27 21:17:17 -0300

    Resolvendo erro de não ter o return

--- vtex/src/App.tsx
+++ vtex/src/App.tsx
@@ -1,5 +1,8 @@
-function App() {
-  return <></>;
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/root";
+
+function App(): React.ReactNode {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
